a299b2230d875e7a0d233d1ce1975726768d1654
xbakhromjon <xbakhromjon@gmail.com>
2022-11-28 06:18:54 +0500

    init

---
+++ src/main/java/uz/bakhromjon/ClassName.java
@@ -0,0 +1,10 @@
+package uz.bakhromjon;
+
+/**
+ * @author : Bakhromjon Khasanboyev
+ **/
+public class ClassName {
+    public static void main(String[] args) {
+        // program statement
+    }
+}
