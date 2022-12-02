89037b609faf932532eca0ee7b83a085040c6f87
xbakhromjon <xbakhromjon@gmail.com>
2022-12-02 08:49:32 +0500

    char

--- src/main/java/uz/bakhromjon/dataTypes/Main3.java
+++ src/main/java/uz/bakhromjon/dataTypes/Main3.java
@@ -5,6 +5,17 @@ package uz.bakhromjon.dataTypes;
  **/
 public class Main3 {
     public static void main(String[] args) {
+        // char 2 byte 16 bit [0, 2^16 - 1 = 65535]
         System.out.println((char) 8482);
+        System.out.println("hello\b");
+        System.out.println("hello\thi");
+        System.out.println("hello \nhi");
+        System.out.println("hello\rhi");
+        System.out.println("hello \"hi July\"");
+        System.out.println("hello 'hi 'July' what's up'");
+        System.out.println("hello\\");
+
+        System.out.println('\u0022' + "nima gap" + '\u0022');
+        // \u000a //  is a newline
     }
 }
