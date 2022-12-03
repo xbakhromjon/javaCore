0f2f5237f501e234caa4be5a638523e4b8cfad21
xbakhromjon <xbakhromjon@gmail.com>
2022-12-03 09:12:55 +0500

    operators

---
+++ src/main/java/uz/bakhromjon/Main.java
@@ -0,0 +1,11 @@
+package uz.bakhromjon;
+
+/**
+ * @author : Bakhromjon Khasanboyev
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println(2.00 - 1.10);
+    }
+}
