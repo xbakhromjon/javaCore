1362a1568d4c52bc67baf98426da4f677687dd5d
xbakhromjon <xbakhromjon@gmail.com>
2022-12-04 10:30:59 +0500

    casts

---
+++ src/main/java/uz/bakhromjon/operators/combiningAssignment/Main.java
@@ -0,0 +1,19 @@
+package uz.bakhromjon.operators.combiningAssignment;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 12;
+//        i = i + 2;
+        i += 2;
+        i -= 2;
+        i *= 2;
+        i /= 2;
+        i %= 2;
+        i ^= 2;
+
+        i += 3.5;
+//        i = (int) (i + 3.5);
+//        i = i + 3.5F;
+        System.out.println(i);
+    }
+}
