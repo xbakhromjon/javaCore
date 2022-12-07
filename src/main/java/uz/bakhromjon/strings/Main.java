77837a8fa82c77435651a4df237695303d68bfc4
xbakhromjon <xbakhromjon@gmail.com>
2022-12-07 07:38:25 +0500

    Strings

--- src/main/java/uz/bakhromjon/strings/Main.java
+++ src/main/java/uz/bakhromjon/strings/Main.java
@@ -32,5 +32,31 @@ public class Main {
 
         String s3 = null;
         System.out.println(s3 != null && s3.length() != 0);
+
+
+        // code point and code units
+        System.out.println("----------------------");
+        String greeting = "Hello";
+        int l = greeting.length();
+        System.out.println(l);
+        System.out.println(greeting.codePointCount(0, l));
+        System.out.println(greeting.codePointAt(0));
+        System.out.println('H' + 0);
+        char h = greeting.charAt(0);
+
+        str = "\uD835\uDD46 is the set of octonions.";
+        int i = str.codePointAt(1);
+        System.out.println(i);
+        System.out.println(Character.isSupplementaryCodePoint(i));
+        System.out.println(Character.isSurrogate(str.charAt(1)));
+        System.out.println(str.substring(0, 2));
+        for (int j = 0; j < str.length(); j++) {
+            if (Character.isSupplementaryCodePoint(str.codePointAt(j))) {
+                j++;
+                continue;
+            }
+            System.out.println(str.charAt(j));
+        }
     }
+
 }
